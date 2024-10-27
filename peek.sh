af019412e6e9eaa627cf64b38c90e48c47689faa
LeyaoJin <leyao.jin23@gmail.com>
2024-10-27 20:42:19 +0100

    third commit

--- peek.sh
+++ peek.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-head -n "$2" "$1"
-echo "..."
-tail -n "$2" "$1"
+
+num_lines=${2:-3}
+
+total_lines=$(cat "$1" | wc -l)
+
+if [[ total_lines <= 2 * num_lines ]]; then
+    cat "$1"
+else
+    echo "Warning: The file contains more than $((2 * num_lines)) lines."
+    head -n "$num_lines" "$1"
+    echo "..."
+    tail -n "$num_lines" "$1"
+fi
 
